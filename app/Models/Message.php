584c9f1527f506a8895bad33c649a4b542947d7f
GustaRibeiror <gustarsoftwr@gmail.com>
2026-05-08 17:57:46 -0300

    provisional models

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
-    //
+    protected $fillable = ['content', 'from', 'user_id'];
+
+    //protected $casts = ['$this->from = ENUM::class']; - chutando pq não sei como fazer
+
+    // protected function casts(): array
+    // {
+    //     return [
+    //         'from' => ENUM::class,
+    //     ];
+    // } outra tentativa
+
+    public function users(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
-{
-    //
+{   
+    protected $fillable = ['description', 'amount', 'type', 'user_id', 'category_id'];
+    
+    public function categorie(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
